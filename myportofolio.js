691726c4d0fa9a62e2caf412b5ca1f0e092a75e4
sudheer944066 <yalmarthisivasaisudheer@gmail.com>
2025-07-19 00:55:41 +0530

    changes

---
+++ myportofolio.js
@@ -0,0 +1,9 @@
+const sandwitch = document.getElementById("sandwitch");
+const overlay = document.getElementById("overlay");
+const back = document.getElementById("back");
+sandwitch.addEventListener("click",()=>{
+        overlay.style.width="100%";
+})
+back.addEventListener("click",()=>{
+        overlay.style.width="0%";
+})
